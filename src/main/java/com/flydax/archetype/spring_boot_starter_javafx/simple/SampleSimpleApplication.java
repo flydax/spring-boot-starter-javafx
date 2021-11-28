196cbcf5bcd431072afbf6f552230f138f64b6e5
git <eae966@gmail.com>
2021-11-28 21:22:55 +0330

    Some updates.

--- src/main/java/com/flydax/archetype/spring_boot_starter_javafx/simple/SampleSimpleApplication.java
+++ src/main/java/com/flydax/archetype/spring_boot_starter_javafx/simple/SampleSimpleApplication.java
@@ -16,14 +16,11 @@
 
 package com.flydax.archetype.spring_boot_starter_javafx.simple;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import com.flydax.archetype.spring_boot_starter_javafx.simple.service.HelloWorldService;
 
 @Configuration
 @EnableAutoConfiguration
@@ -34,12 +31,9 @@ public class SampleSimpleApplication implements CommandLineRunner {
 	// injected bean service. Also demonstrates how you can use @Value to inject
 	// command line args ('--name=whatever') or application properties
 
-	@Autowired
-	private HelloWorldService helloWorldService;
-
 	@Override
 	public void run(String... args) {
-		System.out.println(this.helloWorldService.getHelloMessage());
+		
 	}
 
 	public static void main(String[] args) throws Exception {
